4c74a12cf4edda9b8656d3c5a989f7ac5c2bdad4
David Sobreira Gouvea <davidsgbr@gmail.com>
2017-07-16 18:34:36 +0200

    Scroll to top after click on link

--- src/website/Page.js
+++ src/website/Page.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './Page.css';
 
 class Page extends Component {
+  componentDidMount(prevProps) {
+      window.scrollTo(0, 0);
+  }
+
   render() {
     return (
       <div className="page">
