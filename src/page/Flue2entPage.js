69201d7cb9c32b9b0d453a249a636f03dc824a2a
David Sobreira Gouvea <davidsgbr@gmail.com>
2017-12-27 16:40:56 +0100

    add flue2ent docs

--- src/page/Flue2entPage.js
+++ src/page/Flue2entPage.js
@@ -12,11 +12,81 @@ const info = `
 `;
 
 const installation = `
+### Maven
+\`\`\`
+<dependency>
+  <groupId>org.definitylabs.flue2ent</groupId>
+  <artifactId>flue2ent-core</artifactId>
+  <version>1.0.0</version>
+  <scope>test</scope>
+</dependency>
+\`\`\`
 
+### Gradle
+\`\`\`
+testCompile group: 'org.definitylabs.flue2ent', name: 'flue2ent-core', version: '1.0.0'
+\`\`\`
 `;
 
 const gettingStarted = `
+First step is get an instance of Website based on Selenium WebDriver.
+\`\`\`
+WebDriver driver = ...; // get web driver
 
+String url = "<your website>";
+Website website = Website.with(driver).visit(url);
+\`\`\`
+
+After that, the website can wait for page load or maybe wait until some element has been found.
+\`\`\`
+website.justWait().until(Website.isLoad());
+or
+website.justWait().until(website.hasFound(element(By.tagName("h1"))));
+\`\`\`
+
+The Website is able find elements like WebDriver.
+\`\`\`
+String header = website.findElement(By.tagName("h1")).text();
+\`\`\`
+
+Need a screenshot? Why not!?
+\`\`\`
+website.screenshot().take();
+\`\`\`
+By default it saves at \`./screenshot/screenshot_yyyyMMddHHmmssSSS.png\`. Don't you like it? No problem, there are few other options available.
+
+### Page Object
+[Page Object](https://martinfowler.com/bliki/PageObject.html) is the most common pattern for e2e tests.
+It can be easily implemented using flue2ent.
+
+\`\`\`
+public class PageHeader extends PageObjectDsl<PageHeader> {
+    private PageHeader() {
+    }
+
+    public static PageHeader pageHeader() {
+        return new PageHeader();
+    }
+
+    public WebElementWrapper title() {
+        return website().findElement(By.tagName("h1"));
+    }
+}
+...
+String header = website.at(pageHeader()).title().text();
+\`\`\`
+
+Too many lines? It could be even easier.
+\`\`\`
+public interface PageHeader {
+    @FindElementBy(tagName = "h1")
+    WebElementWrapper title();
+}
+...
+String header = website.at(PageHeader.class).title().text();
+\`\`\`
+
+There are many other interesting features. Please take a look at [User Guide](#/products/flue2ent/docs).
 `;
 
 const userGuide = `
